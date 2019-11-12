b169bc49f8d0d0ada13b6cf1fc63317f1ed5d547
Ayoub <ayoub@aabass.net>
2019-11-12 21:18:42 +0100

    ➕ types

--- scripts/parse-pokemon-data.js
+++ scripts/parse-pokemon-data.js
@@ -1,39 +1,71 @@
-import { writeFile } from 'fs';
+const fs = require('fs');
+const csv = require('csvtojson');
+const pathToPokemonCsv = './data/csv/pokemon.csv';
+const pathToTypesCsv = './data/csv/types.csv';
+const pathToPkmnToTypesCsv = "./data/csv/pokemon_types.csv";
 
-const pathToCsv = './data/csv/pokemon.csv';
-import csv from 'csvtojson';
-
-
-csv().fromFile(pathToCsv).subscribe((resultRow) => {
-  var name = resultRow.identifier;
-  delete resultRow.identifier;
-  resultRow.name = name;
-  if (resultRow.species_id < 650) {
-    resultRow.sprites = {
-      normal: 'http://img.pokemondb.net/sprites/black-white/normal/' + name + '.png',
-      animated: 'http://img.pokemondb.net/sprites/black-white/anim/normal/' + name + '.gif'
-    };
-  } else {
-    resultRow.sprites = {
-      normal: 'http://img.pokemondb.net/sprites/x-y/normal/' + name + '.png'
-    };
+const getGame = (id, isSprite) => {
+  if (isSprite && id < 650) {
+    return 'black-white';
   }
-})
-  .then(obj => {
-    console.log('Writing pokemon data...');
-    return new Promise((resolve, reject) => {
-      writeFile('./lib/pokemon.json', JSON.stringify(obj), (error) => {
-        if (error) {
-          reject(error);
-        } else {
-          resolve('File created');
-        }
-      });
+  if (id < 722) {
+    return 'x-y';
+  }
+  return 'ultra-sun-ultra-moon';
+};
+
+async function parsePokemonData() {
+  const pokemon = await csv()
+    .fromFile(pathToPokemonCsv)
+    .subscribe(resultRow => {
+      var name = resultRow.identifier;
+      delete resultRow.identifier;
+      resultRow.name = name;
+      resultRow.sprites = {
+        icon: `http://img.pokemondb.net/sprites/sun-moon/icon/${name}.png`,
+        sprite: {
+          normal: `http://img.pokemondb.net/sprites/${getGame(
+            resultRow.id,
+            true
+          )}/normal/${name}.png`,
+          animated: `http://img.pokemondb.net/sprites/${getGame(
+            resultRow.id,
+            true
+          )}/anim/normal/${name}.gif`
+        },
+        model: `http://img.pokemondb.net/sprites/${getGame(
+          resultRow.id,
+          false
+        )}/normal/${name}.png`
+      };
+      resultRow.type = [];
     });
-  })
-  .then(result => {
-    return console.log(result);
-  })
-  .catch(error => {
-    return console.error(error);
+  
+  const types = await csv().fromFile(pathToTypesCsv).subscribe(resultRow => {
+    var name = resultRow.identifier;
+    delete resultRow.identifier;
+    resultRow.name = name;
   });
+  
+  const typeConnections = await csv().fromFile(pathToPkmnToTypesCsv);
+
+  const typedPokemon = [...pokemon];
+
+  typeConnections.map(({ pokemon_id: pkmnId, type_id: typeId, slot }) => {
+    if (parseInt(pkmnId) > 1000) return; 
+    typedPokemon[parseInt(pkmnId) - 1].type[parseInt(slot - 1)] = types.filter(({id}) => id === typeId)[0].name;
+  })
+
+  console.log("Writing pokemon data...");
+  return new Promise((resolve, reject) => {
+    fs.writeFile("./lib/pokemon.json", JSON.stringify(typedPokemon), error => {
+      if (error) {
+        reject(error);
+      } else {
+        resolve("File created");
+      }
+    });
+  });
+}
+
+parsePokemonData();
